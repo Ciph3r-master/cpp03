e6ea457737702e61ce839f180b935fe275aef151
Ciph3r-master <quentintruchet24@gmail.com>
2025-11-26 15:01:19 +0100

    Changed main.cpp

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -7,39 +7,39 @@
 int	main()
 {
 	DiamondTrap def;
-	// DiamondTrap name("NAME");
-	// DiamondTrap copy(name);
-	// DiamondTrap tmp("copy");
-	// DiamondTrap opCopy;
+	DiamondTrap name("NAME");
+	DiamondTrap copy(name);
+	DiamondTrap tmp("copy");
+	DiamondTrap opCopy;
 
-	// opCopy = tmp;
+	opCopy = tmp;
 
-	// def.debug();
-	// name.debug();
-	// copy.debug();
-	// opCopy.debug();
+	def.debug();
+	name.debug();
+	copy.debug();
+	opCopy.debug();
 
-	// def.guardGate();
-	// def.highFivesGuys();
-	// copy.attack("Bernard");
+	def.guardGate();
+	def.highFivesGuys();
+	copy.attack("Bernard");
 
-	// std::cout << "-----------------\n";
-	// def.whoAmI();
-	// std::cout << "-----------------\n";
-	// std::cout << "-----------------\n";
-	// name.whoAmI();
-	// std::cout << "-----------------\n";
-	// std::cout << "-----------------\n";
-	// copy.whoAmI();
-	// std::cout << "-----------------\n";
-	// std::cout << "-----------------\n";
-	// opCopy.whoAmI();
-	// std::cout << "-----------------\n";
+	std::cout << "-----------------\n";
+	def.whoAmI();
+	std::cout << "-----------------\n";
+	std::cout << "-----------------\n";
+	name.whoAmI();
+	std::cout << "-----------------\n";
+	std::cout << "-----------------\n";
+	copy.whoAmI();
+	std::cout << "-----------------\n";
+	std::cout << "-----------------\n";
+	opCopy.whoAmI();
+	std::cout << "-----------------\n";
 
-	// tmp.takeDamage(15);
-	// tmp.debug();
+	tmp.takeDamage(15);
+	tmp.debug();
 
-	// opCopy = tmp;
+	opCopy = tmp;
 
-	// opCopy.debug();
+	opCopy.debug();
 }
